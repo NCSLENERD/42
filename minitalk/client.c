813cb3e72e0a06ad38f4810314f4f46e15d13498
ncslenerd <ncslenerd@DESKTOP-00FA3AV.localdomain>
2026-04-12 15:26:41 +0200

    final version minitalk with bonus

--- minitalk/client.c
+++ minitalk/client.c
@@ -11,60 +11,60 @@
 /* ************************************************************************** */
 #include "minitalk.h"
 
-volatile sig_atomic_t	flag;
+volatile sig_atomic_t	g_flag;
 
-void    send_char(char c, int pid)
+void	send_char(char c, int pid)
 {
-    int bit;
+	int	bit;
 
-    bit = 0;
-    while (bit < 8)
-    {
-        if ((c >> bit) & 1)
-            kill(pid,SIGUSR2);
-        else
-            kill(pid,SIGUSR1);
-        bit++;
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c >> bit) & 1)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		bit++;
 		usleep(500);
-    }
-	
+	}
 	if (c == '\0')
 	{
-   		while (!flag)
-        	usleep(500);
-    	flag = 0;
+		while (!g_flag)
+			usleep(500);
+		g_flag = 0;
 	}
 }
 
 void	handler_ack(int sig)
 {
 	(void)sig;
-	flag = 1;
+	g_flag = 1;
 	ft_printf("Ack recu cote client !\n");
 }
 
-void    decomp(char *s, int pid)
+void	decomp(char *s, int pid)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i])
-    {
-        send_char(s[i], pid);
-        i++;
-    }
+	i = 0;
+	while (s[i])
+	{
+		send_char(s[i], pid);
+		i++;
+	}
 	send_char('\0', pid);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    int pid;
-	flag = 0;
-	signal(SIGUSR1,handler_ack);
-    if (argc == 3)
-    {
-        pid = ft_atoi(argv[1]);
-        decomp(argv[2], pid);
-    }
-    return (0);
+	int	pid;
+
+	g_flag = 0;
+	signal(SIGUSR1, handler_ack);
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		decomp(argv[2], pid);
+	}
+	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -11,47 +11,38 @@
 /* ************************************************************************** */
 #include "minitalk.h"
 
-void handler(int sig, siginfo_t *info, void *context)
+void	handler(int sig, siginfo_t *info, void *context)
 {
-    static int      bits;
-    static char     c;
+	static int		bits;
+	static char		c;
 	pid_t			sender_pid;
- 
+
 	(void)context;
 	sender_pid = info->si_pid;
 	if (sig == SIGUSR2)
 		c |= (1 << bits);
-    bits++;
-	if(c == '\0')
+	bits++;
+	if (bits == 8)
 	{
-		kill(sender_pid, SIGUSR1);
-		bits = 0;
-	}
-    if (bits == 8)
-    {
-		if(c == '\0')
-		{
+		if (c == '\0')
 			kill(sender_pid, SIGUSR1);
-			bits = 0;
-		}
 		else
-		{
-        	write(1, &c, 1);
-       		bits = 0;
-        	c = 0;
-		}
-    }
+			write(1, &c, 1);
+		bits = 0;
+		c = 0;
+	}
 }
-//TODO FAIRE L ' ACK
-int main(void)
+
+int	main(void)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
+
 	sa.sa_sigaction = handler;
 	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask); 
-	ft_printf("%d\n",getpid());
+	sigemptyset(&sa.sa_mask);
+	ft_printf("%d\n", getpid());
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-	while(1)
+	while (1)
 		pause();
 }
