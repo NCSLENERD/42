3346c88355184317232486d74a6c8abba8daf6fe
Nicolas Mayela <nmayela@f2r9s4.clusters.42paris.fr>
2026-04-27 19:54:41 +0200

    remove bad indnetations in so_long.c and 2 useless lines

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -18,48 +18,45 @@ int	close_win(void *param)
 	return (0);
 }
 
-void  init_game(t_game *game)                                   
-  {                                                                                                                                                                                                                                           
+void  init_game(t_game *game)
+  {
       game->mlx = NULL;
-      game->win = NULL;                                                                                                                                                                                                                       
-      game->map = NULL;                                           
+      game->win = NULL;
+      game->map = NULL;
       game->map_width = 0;
-      game->map_height = 0;                                                                                                                                                                                                                   
+      game->map_height = 0;
       game->player_x = 0;
-      game->player_y = 0;                                                                                                                                                                                                                     
-      game->collect_remain = 0;                                   
+      game->player_y = 0;
+      game->collect_remain = 0;
       game->nbmoves = 0;
 }
 
-int  main(int argc, char *argv[])                                                                                                                                                                                                           
-{                                                                                                                                                                                                                                           
-	t_game  game;   
-	int	i;                                                                                                                                                                                                                   
-                                                                  
-	/* 1. init la struct */                                                                                                                       
-	game.mlx = NULL;
-    game.win = NULL;
-	init_game(&game);                                          
-    if (argc != 2)                                                                                                                                                                                                                          
-		error_exit(&game, "Too few or too many arguments");                                                                                                                                                                                 
+int  main(int argc, char *argv[])
+{
+	t_game  game;
+	int	i;
+	
+	init_game(&game);
+    if (argc != 2)
+		error_exit(&game, "Too few or too many arguments");
     if (!verif_files(argv[1]))
-		error_exit(&game, "Invalid file name or extension");                                                                                                                                                                                
-    init_map(&game, argv[1]);                                   
-	if (!verif_map(&game))                                                                                                                                                                                                                  
+		error_exit(&game, "Invalid file name or extension");
+    init_map(&game, argv[1]);
+	if (!verif_map(&game))
 		error_exit(&game, "Invalid map");
 	i = 0;
 	while (i < game.map_height)
 	{
 		printf("%s\n", game.map[i]);
 		i++;
-	}                                                                                                                                                                                               
+	}
     game.mlx = mlx_init();
-    game.win = mlx_new_window(game.mlx, 800, 600, "so_long");                                                                                                                                                                               
+    game.win = mlx_new_window(game.mlx, 800, 600, "so_long");
     mlx_hook(game.win, 17, 0, close_win, game.mlx);
-	/* 4. (Phase 2 : load_textures + render_map ici) */                                                                                                                                                                                     
-                                                                                                                                                                                                                                              
-    /* 5. Lancer la boucle (BLOQUANT) */                                                                                                                                                                                                    
-    mlx_loop(game.mlx);                                                                                                                                                                                                                                                                                                                                                                                                                                               
+	/* 4. (Phase 2 : load_textures + render_map ici) */
+
+    /* 5. Lancer la boucle (BLOQUANT) */
+    mlx_loop(game.mlx);
     free_game(&game);
-    return (0);                                                                                                                                                                                                                             
+    return (0);
   }
