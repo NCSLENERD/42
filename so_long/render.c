bbaf8d94507dfe460dca3792f3e3ef2b8f8dc586
Nicolas Mayela <nmayela@f2r10s20.clusters.42paris.fr>
2026-04-28 17:14:57 +0200

    fix bugs in render.c

--- so_long/render.c
+++ so_long/render.c
@@ -15,24 +15,27 @@
 void    load_textures(t_game *game)
 {
     void    *img;
+    int     h;
+    int     w;
 
-    img = mlx_xpm_file_to_image(game->mlx, "/textures/floor.xpm", 64, 64);
+
+    img = mlx_xpm_file_to_image(game->mlx, "textures/floor.xpm", &w, &h);
     if (!img)
         error_exit(game, "Failed to load floor texture");
     game->img_floor = img;
-    img = mlx_xpm_file_to_image(game->mlx, "/textures/collect.xpm", 64, 64);
+    img = mlx_xpm_file_to_image(game->mlx, "textures/collect.xpm", &w, &h);
     if (!img)
         error_exit(game, "Failed to load collect texture");
     game->img_collect = img;
-    img = mlx_xpm_file_to_image(game->mlx, "/textures/player.xpm", 64, 64);
+    img = mlx_xpm_file_to_image(game->mlx, "textures/player.xpm", &w, &h);
     if (!img)
         error_exit(game, "Failed to load player texture");
     game->img_player = img;
-    img = mlx_xpm_file_to_image(game->mlx, "/textures/exit.xpm", 64, 64);
+    img = mlx_xpm_file_to_image(game->mlx, "textures/exit.xpm", &w, &h);
     if (!img)
         error_exit(game, "Failed to load exit texture");
     game->img_exit = img;
-    img = mlx_xpm_file_to_image(game->mlx, "/textures/wall.xpm", 64, 64);
+    img = mlx_xpm_file_to_image(game->mlx, "textures/wall.xpm", &w, &h);
     if (!img)
         error_exit(game, "Failed to load wall texture");
     game->img_wall = img;
@@ -43,15 +46,15 @@ void    render_tile(t_game *game, int x, int y)
     char c;
 
     c = game->map[y][x];
-    mlx_xpm_file_to_window(game->mlx, game->win, game->img_floor, x * TILE_SIZE, y *TILE_SIZE);
+    mlx_put_image_to_window(game->mlx, game->win, game->img_floor, x * TILE_SIZE, y *TILE_SIZE);
     if(c == '1')
-        mlx_xpm_file_to_window(game->mlx, game->win, game->img_wall, x * TILE_SIZE, y *TILE_SIZE);
+        mlx_put_image_to_window(game->mlx, game->win, game->img_wall, x * TILE_SIZE, y *TILE_SIZE);
     else if(c == 'P')
-        mlx_xpm_file_to_window(game->mlx, game->win, game->img_player, x * TILE_SIZE, y *TILE_SIZE);
+        mlx_put_image_to_window(game->mlx, game->win, game->img_player, x * TILE_SIZE, y *TILE_SIZE);
     else if(c == 'C')
-        mlx_xpm_file_to_window(game->mlx, game->win, game->img_collect, x * TILE_SIZE, y *TILE_SIZE);
+        mlx_put_image_to_window(game->mlx, game->win, game->img_collect, x * TILE_SIZE, y *TILE_SIZE);
     else if(c == 'E')
-        mlx_xpm_file_to_window(game->mlx, game->win, game->img_exit, x * TILE_SIZE, y *TILE_SIZE);
+        mlx_put_image_to_window(game->mlx, game->win, game->img_exit, x * TILE_SIZE, y *TILE_SIZE);
 }
 
 void    render_map(t_game *game)
